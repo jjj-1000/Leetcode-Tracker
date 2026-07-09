950eccd1030ee6beeed69ea11849e20fc88998bd
Janani <jananinila04@gmail.com>
2026-07-09 15:12:53 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 3:12:53 PM
+class Solution {
+    public int singleNumber(int[] nums) {
+       
+        for(int i=0;i<nums.length;i++){
+           int c=0;
+           for(int j=0;j<nums.length;j++){
+            if(nums[i]==nums[j]){
+                c++;
+            }
+           }
+           if(c==1){
+            return nums[i];
+           }
+        }
+       return -1;
+    }
+}
