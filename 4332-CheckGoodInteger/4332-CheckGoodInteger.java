228cb6d72ade223d5ecfa1453559aebfa310719e
Janani <jananinila04@gmail.com>
2026-07-09 15:05:44 +0530

    Create 4332-CheckGoodInteger

---
+++ 4332-CheckGoodInteger/4332-CheckGoodInteger.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 3:05:44 PM
+class Solution {
+    public boolean checkGoodInteger(int n){
+        int sum=0;
+        int sqsum=0;
+        while(n>0){
+            int d=n%10;
+            sum+=d;
+            sqsum+=d*d;
+            n=n/10;
+        }
+        return (sqsum-sum)>=50;
+    }
+}
