e8fb5446a17dc9063b62bfccb42a4b83d4aa462b
Janani <jananinila04@gmail.com>
2026-07-09 15:11:05 +0530

    Create 349-IntersectionOfTwoArrays

---
+++ 349-IntersectionOfTwoArrays/349-IntersectionOfTwoArrays.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 3:11:05 PM
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        boolean[] flag = new boolean[1001];
+
+        for (int num : nums1) {
+            flag[num] = true;
+        }
+
+        int[] result = new int[Math.min(nums1.length, nums2.length)];
+        int index = 0;
+
+        for (int num : nums2) {
+            if (flag[num]) {
+                result[index++] = num;
+                flag[num] = false;
+            }
+        }
+
+        return Arrays.copyOf(result, index);
+    }
+}
