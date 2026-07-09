9702f415dc7f828f3ea3a8ad4bd3a65ae700c07a
Janani <jananinila04@gmail.com>
2026-07-09 15:14:00 +0530

    Create 51-NQueens

---
+++ 51-NQueens/51-NQueens.java
@@ -0,0 +1,76 @@
+// Last updated: 7/9/2026, 3:14:01 PM
+import java.util.*;
+
+class Solution {
+    List<List<String>> ans = new ArrayList<>();
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
+        solve(board, 0, n);
+
+        return ans;
+    }
+
+    private void solve(char[][] board, int row, int n) {
+
+        if (row == n) {
+            List<String> temp = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                temp.add(new String(board[i]));
+            }
+
+            ans.add(temp);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+
+            if (isSafe(board, row, col, n)) {
+
+                board[row][col] = 'Q';
+
+                solve(board, row + 1, n);
+
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int row, int col, int n) {
+
+        
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        
+        for (int i = row - 1, j = col - 1;
+             i >= 0 && j >= 0;
+             i--, j--) {
+
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // right diagonal
+        for (int i = row - 1, j = col + 1;
+             i >= 0 && j < n;
+             i--, j++) {
+
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
