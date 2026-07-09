0c0c09888bf328a5bfc872d9a3726471e6528fcc
Janani <jananinila04@gmail.com>
2026-07-09 15:11:31 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 3:11:32 PM
+class Solution {
+    public void moveZeroes(int[] nums) {
+       int j=0;
+     for(int i=0;i<nums.length;i++){
+         if(nums[i]!=0) {
+            int t=nums[i];
+            nums[i]=nums[j];
+            nums[j]=t;
+            j++;
+
+         }
+     }
+    }
+}
