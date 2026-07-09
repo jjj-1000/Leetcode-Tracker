3ef509f917b90dd550cd2924c8dfd7cf97b32717
Janani <jananinila04@gmail.com>
2026-07-09 15:10:57 +0530

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 3:10:57 PM
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for(int i=0;i<a.length;i++){
+            
+                if(a[i]!=b[i]) return b[i];
+
+            
+        }
+        return b[b.length-1];
+    }
+}
