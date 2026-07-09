d5c3146199cd57a4efcdf7bfbc877c165f4afb8d
Janani <jananinila04@gmail.com>
2026-07-09 15:14:26 +0530

    Create 32-LongestValidParentheses

---
+++ 32-LongestValidParentheses/32-LongestValidParentheses.java
@@ -0,0 +1,28 @@
+// Last updated: 7/9/2026, 3:14:26 PM
+import java.util.Stack;
+
+class Solution {
+    public int longestValidParentheses(String s) {
+            Stack<Integer> stack = new Stack<>();
+                    stack.push(-1);   // Base index
+
+                            int maxLength = 0;
+
+                                    for (int i = 0; i < s.length(); i++) {
+
+                                                if (s.charAt(i) == '(') {
+                                                                stack.push(i);
+                                                                            } else {
+                                                                                            stack.pop();
+
+                                                                                                            if (stack.isEmpty()) {
+                                                                                                                                stack.push(i);
+                                                                                                                                                } else {
+                                                                                                                                                                    maxLength = Math.max(maxLength, i - stack.peek());
+                                                                                                                                                                                    }
+                                                                                                                                                                                                }
+                                                                                                                                                                                                        }
+
+                                                                                                                                                                                                                return maxLength;
+                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                    }
