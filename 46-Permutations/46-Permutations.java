34d418f394afe4466ec44e79bb4762d6df3c087f
Janani <jananinila04@gmail.com>
2026-07-09 15:14:09 +0530

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.java
@@ -0,0 +1,28 @@
+// Last updated: 7/9/2026, 3:14:09 PM
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>(); 
+    public List<List<Integer>> permute(int[] nums) {
+       permutations(0,nums);
+       return ans;
+    }
+    void permutations( int i,int nums[]){
+        if(i==nums.length-1){
+              List<Integer> temp = new ArrayList<>();
+              for(int k=0;k<nums.length;k++){
+                temp.add(nums[k]);
+              }
+           ans.add(temp);
+        }
+        
+        for(int j=i;j<nums.length;j++){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            permutations(i+1,nums);
+            temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+
+        }
+    }
+}
