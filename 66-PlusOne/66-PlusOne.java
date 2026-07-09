59c98734ee5f38490476e8b3f453780413f0901b
Janani <jananinila04@gmail.com>
2026-07-09 15:13:44 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 3:13:44 PM
+class Solution {
+    public int[] plusOne(int[] d) {
+        int n=d.length;
+        for(int i=n-1;i>=0;i--){
+            if(d[i]<9){
+                d[i]++;
+                return d;
+            }
+            d[i]=0;
+        }
+        int [] res= new int[n+1];
+        res[0]=1;
+        return res;
+
+    }
+}
