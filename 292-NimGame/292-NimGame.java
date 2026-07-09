04901330a4195dda1312adb74069af1ca44f30b7
Janani <jananinila04@gmail.com>
2026-07-09 15:11:28 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 3:11:28 PM
+class Solution {
+    public boolean canWinNim(int n) {
+       if(n%4==0){
+        return false;
+       }else{
+        return true;
+       }
+    
+
+
+
+
+    }
+}
