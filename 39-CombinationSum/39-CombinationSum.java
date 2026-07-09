315231cdcf2b193239a3e8d4c09c35b2c0aa8d8b
Janani <jananinila04@gmail.com>
2026-07-09 15:14:17 +0530

    Create 39-CombinationSum

---
+++ 39-CombinationSum/39-CombinationSum.java
@@ -0,0 +1,27 @@
+// Last updated: 7/9/2026, 3:14:18 PM
+class Solution {
+    public void fun(List<Integer> v,int i,int sum,List<Integer> v2,List<List<Integer>> ans){
+        if(sum==0){
+            ans.add(new ArrayList<>(v2));
+            return;
+        }
+        if(sum<0 || i<0){
+            return;
+        }
+        fun(v,i-1,sum,v2,ans);
+        v2.add(v.get(i));
+        fun(v,i,sum-v.get(i),v2,ans);
+        v2.remove(v2.size()-1);
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+         List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> v = new ArrayList<>();
+        for (int num : candidates) {
+            v.add(num);
+        }
+         fun(v, v.size() - 1, target, new ArrayList<>(), ans);
+        
+        return ans;
+        
+    }
+}
