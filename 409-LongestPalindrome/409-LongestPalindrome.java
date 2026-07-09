db5c9f9c1d1ac565b0ad2962d9bf2f4ecbfe6b33
Janani <jananinila04@gmail.com>
2026-07-09 15:10:42 +0530

    Create 409-LongestPalindrome

---
+++ 409-LongestPalindrome/409-LongestPalindrome.java
@@ -0,0 +1,28 @@
+// Last updated: 7/9/2026, 3:10:42 PM
+class Solution {
+    public int longestPalindrome(String s) {
+        HashMap<Character, Integer> charCount = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        int length = 0;
+        boolean hasOdd = false;
+
+        for (int count : charCount.values()) {
+            if (count % 2 == 0) {
+                length += count;
+            } else {
+                length += count - 1;
+                hasOdd = true;
+            }
+        }
+
+        if (hasOdd) {
+            length += 1;
+        }
+
+        return length;      
+    }
+}
