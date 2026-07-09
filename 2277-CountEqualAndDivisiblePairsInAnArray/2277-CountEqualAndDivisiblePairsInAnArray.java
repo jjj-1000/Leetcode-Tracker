a9c458e8c713d90dbf3f6ae6e10a78a85cea81d0
Janani <jananinila04@gmail.com>
2026-07-09 15:07:19 +0530

    Create 2277-CountEqualAndDivisiblePairsInAnArray

---
+++ 2277-CountEqualAndDivisiblePairsInAnArray/2277-CountEqualAndDivisiblePairsInAnArray.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 3:07:20 PM
+class Solution {
+    public int countPairs(int[] nums, int k) {
+        int c=0;
+       for(int i=0;i<nums.length;i++){
+        for(int j=i+1;j<nums.length;j++){
+            if((nums[i]==nums[j]) && (i * j %k==0)){
+                c++;
+            }
+        }
+       } 
+       return c;
+    }
+}
